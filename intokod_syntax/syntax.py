7bdf84a83cf49a80e775e2dc807e439bd97f081b
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-08-28 09:37:14 +0200

    New folder and syntax.py fil

---
+++ intokod_syntax/syntax.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+#creating a variable and setting the value "Vincent"
+name = "Vincent"
+#printing it
+print(f"Ditt namn är: {name}")
+
+side = float(input("Ange kvadrates sida: "))
+area = side**2
+
+print(f"Arean är: {area} a.e")
