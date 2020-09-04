fb0c91e1b8df54e9a09d7103637a9780882177a5
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-04 08:35:06 +0200

    uppgifter + codealong folder

---
+++ Programmeringsövningshäfte/uppgift2.py
@@ -0,0 +1,7 @@
+bas = float(input("Basen av triangeln: "))
+höjd = float(input("Höjden av triangeln: "))
+
+kvadrat = bas * höjd
+triangel = kvadrat / 2
+
+print(f"Arean på triangeln är: {triangel}")
