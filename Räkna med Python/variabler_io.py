f2121e13c091bd9a74b68c6258b3a02718587628
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-04 09:37:26 +0200

    beep

--- Räkna med Python/variabler_io.py
+++ Räkna med Python/variabler_io.py
@@ -1,1 +1,16 @@
-print("hej")
+
+#Tyngdkraft = F = mg
+#print("hej")
+
+#g = 9.82
+#m = float(input("Din Vikt: "))
+
+#F = m * g
+
+#print(f"Din Tyngdkraft är: {F}N")
+
+decimalTal = float(input("Ange decimal tal: "))
+
+procent = decimalTal*100
+
+print(f"Decimalen: {decimalTal} I Procent: {procent:.0f}%")
