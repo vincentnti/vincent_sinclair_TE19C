0251deca25fb9e62a704a9827385f2d7ca462010
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-07 19:28:18 +0200

    la till kr till mer priser

--- Räkna med Python/vasttrafik.py
+++ Räkna med Python/vasttrafik.py
@@ -8,6 +8,6 @@ days = int(input("How many days do you wish to travel: "))
 print(f"Price: {dayTicket*days}kr")
 
 print("Is it worth using Day Tickets an entire month instead of Month Tickets?")
-print(f"Price for Day Tickets lasting a month {dayTicket*30.42}. Price for Month Ticket: {monthTicket}. Is it worth using Day Tickets over Month Tickets? {dayTicket*30.42 < monthTicket}")
+print(f"Price for Day Tickets lasting a month {dayTicket*30.42}kr. Price for Month Ticket: {monthTicket}kr. Is it worth using Day Tickets over Month Tickets? {dayTicket*30.42 < monthTicket}")
 
 
