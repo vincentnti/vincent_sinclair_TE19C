7a012cbf980a925640ea46925ea00cbad9887d83
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-11 09:36:49 +0200

    uppgift 3  klar

--- Programmeringsövningshäfte/if_uppgift2.py
+++ Programmeringsövningshäfte/if_uppgift2.py
@@ -4,10 +4,6 @@ if tal % 2 == 0:
     print("Talet är jämt")
 else:
     print("Talet är udda")
+if tal % 5 == 0:
+    print("Talet är delbart med 5")
 
-"""
-inte klar antar jag
-jdfkölafjafkljdfjöjföjlad:
-    print("Talet ä delbart med 5")
-
-"""
---
+++ Programmeringsövningshäfte/if_uppgift3.py
@@ -0,0 +1,9 @@
+tal1 = float(input("Tal 1: "))
+tal2 = float(input("Tal 2: "))
+tal3 = float(input("Tal 3: "))
+tal4 = float(input("Tal 4: "))
+
+list = [tal1,tal2,tal3,tal4]
+list.sort()
+
+print(list[3], "Är störst")
