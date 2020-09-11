c6aa03e48ea4206505d4d2a25a8e886e8efd7e8d
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-11 08:36:50 +0200

    namn ändring

--- Programmeringsövningshäfte/if_uppgift2.py
+++ Programmeringsövningshäfte/if_uppgift2.py
@@ -4,9 +4,9 @@ if tal - int(tal) > 0:
     print("Talet är udda")
 else:
     print("Talet är jämnt")
+
 """
 jdfkölafjafkljdfjöjföjlad:
     print("Talet ä delbart med 5")
 
-    INTE Klar
-    ddddd
+"""
