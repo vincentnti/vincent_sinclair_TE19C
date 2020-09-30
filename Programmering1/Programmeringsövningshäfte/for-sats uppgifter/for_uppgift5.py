80be154cae9fbf4ddab259e020fbf64c4d2ca82e
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-09-30 13:31:44 +0200

    v2 till upg5 och ny upg6 och ny code along

--- Programmering1/Programmeringsövningshäfte/for-sats uppgifter/for_uppgift5.py
+++ Programmering1/Programmeringsövningshäfte/for-sats uppgifter/for_uppgift5.py
@@ -13,6 +13,7 @@ for i in range(start, end):
     print(i * 10)
 """
 #C
+"""
 counter = 1
 x = 0
 for i in range(1,111):
@@ -21,4 +22,12 @@ for i in range(1,111):
     if counter > 10:
         counter = 1
         x += 1
+"""
+#C version 2
+count=0
+for i in range(1,11):
+    count+=1
+    print("\n ",count, "tabell")
+    for j in range(1,11):
+        print(count * j, end=" ")
 
