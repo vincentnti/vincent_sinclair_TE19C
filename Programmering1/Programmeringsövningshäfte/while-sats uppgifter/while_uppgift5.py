8a2fa9b1139a266605bb911dd756d54c60ac19fd
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-10-07 21:25:37 +0200

    Lite extra fluff till uppgift 5

--- Programmering1/Programmeringsövningshäfte/while-sats uppgifter/while_uppgift5.py
+++ Programmering1/Programmeringsövningshäfte/while-sats uppgifter/while_uppgift5.py
@@ -1,18 +1,22 @@
 import random
 import sympy
 random_number = random.randint(0,101)
-#print(random_number)
+print(random_number)
 
 guess = ""
 while guess != random_number:
     guess = int(input("Guess a number between 0 and 100: "))
     if guess == random_number:
         print(f"You got it! The number was {random_number}!")
-        exit
+        break
     elif guess > random_number:
         print("Guess Lower!")
     elif guess < random_number:
         print("Guess Higher!")
     if sympy.isprime(random_number):
         print("It's a prime number!")
+    elif random_number % 2 == 0:
+        print("It's a even number!")
+    else:
+        print("It's a odd number!")
 
