c1a19facb984da3c41752fe35d83ba156b03469e
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-10-16 08:44:13 +0200

    New

---
+++ Programmering1/Spring Lab/Bolt-run.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+sträcka = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+tid = [1.83, 2.87, 3,78, 4.65, 5.5, 6.32, 7.14, 7.96, 8.79, 9.69]
+
+plt.plot(tid, sträcka)
+plt.show()
