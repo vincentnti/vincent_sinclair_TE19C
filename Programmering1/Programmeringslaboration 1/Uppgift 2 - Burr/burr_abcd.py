edd725d4a06aea36d82bd4aa5d3a781a53307b97
SpecsUser <vincent.sinclair255@gmail.com>
2020-10-26 23:08:05 +0100

    Påbörjan av burr

---
+++ Programmering1/Programmeringslaboration 1/Uppgift 2 - Burr/burr_abcd.py
@@ -0,0 +1,18 @@
+#A
+for i in range(1,101):
+    print(i)
+#B
+for i in range(1,101):
+    if i % 5 == 0:
+        print("Burr")
+    else:
+        print(i)
+#C och D
+print("Input a whole number")
+answer = int(input("Input: "))
+if answer % 5 == 0:
+    print("Burr")
+elif answer % 3 == 0:
+    print("Birr")
+else:
+    print(answer)
