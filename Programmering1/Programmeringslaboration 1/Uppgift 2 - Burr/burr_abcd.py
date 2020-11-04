eab83f39175dda52699ec0b445a6a9a04793f4d7
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-11-04 14:14:29 +0100

    nytt

--- Programmering1/Programmeringslaboration 1/Uppgift 2 - Burr/burr_abcd.py
+++ Programmering1/Programmeringslaboration 1/Uppgift 2 - Burr/burr_abcd.py
@@ -10,12 +10,14 @@ for i in range(1,101):
         print(i)
 
 #C och D
-print("Input a whole number")
-answer = int(input("Input: "))
-
-if answer % 5 == 0:
-    print("Burr")
-elif answer % 3 == 0:
-    print("Birr")
-else:
-    print(answer)
+print("Mata in ett tal vars multiplar blir ”Burr”")
+burr = int(input("Input: "))
+print("Mata in ett tal vars multiplar blir ”Birr”")
+birr = int(input("Input: "))
+for i in range(1,101):
+    if i % burr == 0:
+        print("Burr")
+    if i % birr == 0:
+        print("Birr")
+    else:
+        print(i)
