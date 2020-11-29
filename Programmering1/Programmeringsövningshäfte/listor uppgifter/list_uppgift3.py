81aad7e2cfb8ded8194fbeaf84093c73bcb76265
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-11-29 02:22:59 +0100

    Chess board generated

--- Programmering1/Programmeringsövningshäfte/listor uppgifter/list_uppgift3.py
+++ Programmering1/Programmeringsövningshäfte/listor uppgifter/list_uppgift3.py
@@ -1,5 +1,6 @@
-chessboard = []
+rows = []
 chessletters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-for i in range(0,8):
-    row = [ + x for x in range(1,9)]
-#NOT DONE
+for i in range(1,9):
+    rows.append([str(i) + letter for letter in chessletters])
+
+print(rows)
