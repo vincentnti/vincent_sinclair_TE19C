f26e09a73cc4bc622cc0ec6be1ba767dfe773e59
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2020-12-04 09:46:54 +0100

    kommer göras om

--- Programmering1/Programmeringsövningshäfte/funktioner uppgifter/funktion_uppgift2.py
+++ Programmering1/Programmeringsövningshäfte/funktioner uppgifter/funktion_uppgift2.py
@@ -1,16 +1,24 @@
 #a
 def summa(n):
-    return 2 * n + 1
+    answer = 0
+    for i in range(n):
+        answer += 2*i+1
+    return answer
 
 n = int(input("Set n: "))
-print(f"Svaret blir {summa(n)} på position {n}")
+print(f"Svaret blir {summa(n)}")
 #b
 
 ns = int(input("Set n start: "))
 nf = int(input("Set n final: "))
 
 def iter_summa(ns, nf):
-    for i in range(ns, nf + 1):
-        print(f"Svaret blir {summa(i)} på position {i}")
+    answer = 0
+    for i in range(ns + 1,nf):
+        answer += 2*i+1
+    return answer
 
-iter_summa(ns, nf)
+print(f"Svaret blir {iter_summa(ns, nf)}")
+
+#allt ska göras om gjorde fel i början visste inte att
+#man skulle lägga ihop det
