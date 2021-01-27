9d3ea37ffb6e307b68a7132398799ab25f56300b
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2021-01-27 11:40:32 +0100

    print each row \n

--- Programmering1/Programmeringsövningshäfte/listor uppgifter/list_uppgift3.py
+++ Programmering1/Programmeringsövningshäfte/listor uppgifter/list_uppgift3.py
@@ -3,4 +3,5 @@ chessletters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 for i in range(1,9):
     rows.append([str(i) + letter for letter in chessletters])
 
-print(rows)
+for row in rows:
+    print(row)
