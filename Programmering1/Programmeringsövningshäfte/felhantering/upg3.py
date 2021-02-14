ed73999bb9f9f24c264be9f6fd0322a433f35d36
vincentnti <vincent.sinclair@elev.ga.ntig.se>
2021-02-15 00:47:25 +0100

    nu klar med upg 3

--- Programmering1/Programmeringsövningshäfte/felhantering/upg3.py
+++ Programmering1/Programmeringsövningshäfte/felhantering/upg3.py
@@ -4,18 +4,22 @@ ONETIME_PRICE = 100
 while True:
     try:
         print("Ange hur många spårvagns resor du vill ta")
-        trips = int(input("Antal Resor: "))
+        
+        trips = input("Antal Resor: ")
+        trips = trips.replace(" ", "")
+        trips = int(trips)
+
         assert(trips > 0)
         break
     except ValueError as err:
-        print("[X] Måste vara ett nummer")
-    except AssertionError:
+        print("[X] Det du skriver in måste vara ett nummer och ett heltal t.ex 5")
+    except AssertionError as err:
         print("[X] Måste vara fler än 0 resor")
    
 month_price = MONTH_PRICE
 onetime_price = trips * ONETIME_PRICE
 
-print("Månadskostnad:", month_price)
+print("Månadskostnad:", month_price, end=" ")
 print("Engångskostnad:", onetime_price)
 
 if month_price < onetime_price:
